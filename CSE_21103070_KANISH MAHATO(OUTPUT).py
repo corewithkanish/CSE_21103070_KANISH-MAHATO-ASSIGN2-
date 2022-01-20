f07f2799a15f89f98a49b2dac33b06b21948593c
corewithkanish <72293910+corewithkanish@users.noreply.github.com>
2022-01-21 00:07:48 +0530

    Add files via upload

---
+++ CSE_21103070_KANISH MAHATO(OUTPUT).py
@@ -0,0 +1,248 @@
+# Windows PowerShell
+# Copyright (C) Microsoft Corporation. All rights reserved.
+
+# Try the new cross-platform PowerShell https://aka.ms/pscore6
+
+# Warning: PowerShell detected that you might be using a screen reader and has disabled PSReadLine for compatibility purposes. If you want to re-enable it, run 'Import-Module PSReadLine'.
+
+# PS A:\Python> & C:/Users/Asus/AppData/Local/Programs/Python/Python310/python.exe a:/Python/python8.py
+# ------------------------++++++---------------------------
+# 35
+# egaugnal evitisnes esac a si nohtyP
+# Python is  language
+# Python is object oriented language
+# 10
+# Pythonisacasesensitivelanguage
+
+
+# ------------------------++++++---------------------------
+# Your name
+# kanish Mahato
+# Your SID
+# 21103070
+# Your department
+# CSE
+# Your CGPA
+# 9.9
+
+# Hey,kanish Mahato Here!
+# My SID is 21103070
+# I am from CSE department and my CGPA is 9.900000
+
+
+# ------------------------++++++---------------------------
+# 8
+# 58
+# 50
+# 224 40
+# 14 0
+
+
+# ------------------------++++++---------------------------
+# Enter first  your number
+# 12
+# Enter second your number
+# 12
+# Enter third  your number
+# 24
+# Highest number is : 24
+
+
+
+
+# ------------------------++++++---------------------------
+# Yes
+
+
+# ------------------------++++++---------------------------
+# Enter Value of 1st :
+# 12
+# Enter Value of 2nd :
+# 16
+# Enter Value of 3rd :
+# 6
+# Yes,This form triangle
+
+
+# ------------------------++++++---------------------------
+# PS A:\Python> & C:/Users/Asus/AppData/Local/Programs/Python/Python310/python.exe a:/Python/python8.py
+# ------------------------++++++---------------------------
+# 35
+# egaugnal evitisnes esac a si nohtyP
+# Python is  language
+# Python is object oriented language
+# 10
+# Pythonisacasesensitivelanguage
+
+
+# ------------------------++++++---------------------------
+# Your name
+# Kanish Mahato
+# Your SID
+# 21103070
+# Your department
+# CSE
+# Your CGPA
+# 9.9
+
+# Hey,Kanish Mahato Here!
+# My SID is 21103070
+# I am from CSE department and my CGPA is 9.900000
+
+
+# ------------------------++++++---------------------------
+# 8
+# 58
+# 50
+# 224 40
+# 14 0
+
+
+# ------------------------++++++---------------------------
+# Enter first  your number
+# 12
+# Enter second your number
+# 15
+# Enter third  your number
+# 17
+# Highest number is : 17
+
+
+
+
+# ------------------------++++++---------------------------
+# Yes
+
+
+# ------------------------++++++---------------------------
+# Enter Value of 1st :
+# 16
+# Enter Value of 2nd :
+# 26
+# Enter Value of 3rd :
+# 778
+# No,This not form triangle
+
+
+# ------------------------++++++---------------------------
+# PS A:\Python> & C:/Users/Asus/AppData/Local/Programs/Python/Python310/python.exe a:/Python/python8.py
+# ------------------------++++++---------------------------
+# 35
+# egaugnal evitisnes esac a si nohtyP
+# Python is  language
+# Python is object oriented language
+# 10
+# Pythonisacasesensitivelanguage
+
+
+# ------------------------++++++---------------------------
+# Your name
+# kanish Mahato
+# Your SID
+# 21103070
+# Your department
+# CSE
+# Your CGPA
+# 9.9
+
+# Hey,kanish Mahato Here!
+# My SID is 21103070
+# I am from CSE department and my CGPA is 9.900000
+
+
+# ------------------------++++++---------------------------
+# 8
+# 58
+# 50
+# 224 40
+# 14 0
+
+
+# ------------------------++++++---------------------------
+# Enter first  your number
+# 14
+# Enter second your number
+# 67
+# Enter third  your number
+# 89
+# Highest number is : 89
+
+
+
+
+# ------------------------++++++---------------------------
+# Yes
+
+
+# ------------------------++++++---------------------------
+# We using this formula (a + b >=c)  so please enter in this order
+
+# Enter Value of 1st :
+# & C:/Users/Asus/AppData/Local/Programs/Python/Python310/python.exe a:/Python/python8.py
+# Traceback (most recent call last):
+#   File "a:\Python\python8.py", line 72, in <module>
+#     num1 = float(input("Enter Value of a : \n"))
+# ValueError: could not convert string to float: '& C:/Users/Asus/AppData/Local/Programs/Python/Python310/python.exe a:/Python/python8.py'
+# PS A:\Python> & C:/Users/Asus/AppData/Local/Programs/Python/Python310/python.exe a:/Python/python8.py
+# ------------------------++++++---------------------------
+# 35
+# egaugnal evitisnes esac a si nohtyP
+# Python is  language
+# Python is object oriented language
+# 10
+# Pythonisacasesensitivelanguage
+
+
+# ------------------------++++++---------------------------
+# Your name
+# Kanish Mahato
+# Your SID
+# 21103070
+# Your department
+# CSE
+# Your CGPA
+# 9.9
+
+# Hey,Kanish Mahato Here!
+# My SID is 21103070
+# I am from CSE department and my CGPA is 9.900000
+
+
+# ------------------------++++++---------------------------
+# 8
+# 58
+# 50
+# 224 40
+# 14 0
+
+
+# ------------------------++++++---------------------------
+# Enter first  your number
+# 12
+# Enter second your number
+# 78
+# Enter third  your number
+# 95
+# Highest number is : 95
+
+
+
+
+# ------------------------++++++---------------------------
+# Yes
+
+
+# ------------------------++++++---------------------------
+# We using this formula (a + b >=c)  so please enter in this order
+
+# Enter Value of a :
+# 12
+# Enter Value of b :
+# 0
+# Enter Value of c :
+# 78
+# No,This not form triangle
+
+
+# ------------------------++++++---------------------------
+# PS A:\Python>
